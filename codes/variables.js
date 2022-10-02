89df917b85c5e240995f8872cd855b1f55a2531b
Aditya Showrov <adityashowrov1996@gmail.com>
2022-10-02 22:03:32 +0600

    class 2 lessons

--- codes/variables.js
+++ codes/variables.js
@@ -18,3 +18,44 @@ function gg() {
   console.log("newV = " + newV);
 }
 gg();
+
+/**
+Primary Datatypes:
+String, Number, Boolean
+
+Composite Datatypes:
+Array, Object
+
+Special Datatypes:
+Undefined, Null
+
+ */
+console.log("Data Types: \n");
+data;
+console.log(typeof data); //undefined
+data = 8;
+console.log(typeof data); //number
+var data = "StringData";
+console.log(typeof data); //string
+data = null;
+console.log(typeof data); //object
+data = true;
+console.log(typeof data); //boolean
+console.log("-----------");
+
+/*
+
+Difference between null and undefined
+When checking for null or undefined, beware of the differences between equality (==) and identity (===) operators, as the former performs type-conversion.
+
+typeof null          // "object" (not "null" for legacy reasons)
+typeof undefined     // "undefined"
+null === undefined   // false
+null  == undefined   // true
+null === null        // true
+null == null         // true
+!null                // true
+isNaN(1 + null)      // false
+isNaN(1 + undefined) // true
+
+*/
